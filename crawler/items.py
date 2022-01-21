26b23b1c505bfb79c6c8f5e59d116e0a7a09363b
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2022-01-21 16:10:36 -0500

    Added route item

--- crawler/items.py
+++ crawler/items.py
@@ -2,11 +2,11 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+from dataclasses import dataclass
 
-import scrapy
 
-
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dataclass
+class Route:
+    code: str
+    title: str
+    schedule: list
