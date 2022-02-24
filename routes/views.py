3da00d221943706783f1e3698fc5d482ea4ab52d
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2022-02-24 15:27:15 -0500

    SHow all routes on home page

--- routes/views.py
+++ routes/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Route
+
 
 class HomeView(TemplateView):
     template_name = 'routes/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['route_list'] = Route.objects.all()
+        return context
