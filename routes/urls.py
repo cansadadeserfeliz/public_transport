56cc15d1bfd3e8e017f6982f151024952080c71a
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2022-02-24 16:12:33 -0500

    Added view to show route details

--- routes/urls.py
+++ routes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import HomeView
+from .views import RouteDetailView
 
 app_name = 'routes'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('route/<int:pk>/', RouteDetailView.as_view(), name='route_detail'),
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
+from django.views.generic import DetailView
 
 from .models import Route
+from .models import RouteStations
 
 
 class HomeView(TemplateView):
@@ -9,4 +11,20 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['route_list'] = Route.objects.all()
+        return context
+
+
+class RouteDetailView(DetailView):
+    model = Route
+    context_object_name = 'route'
+
+    def get_context_data(self, **kwargs):
+        context = super(RouteDetailView, self).get_context_data(**kwargs)
+        route_1 = self.object.route_stations.filter(
+            direction=RouteStations.DIRECTION_1,
+        ).all()
+        route_2 = self.object.route_stations.filter(
+            direction=RouteStations.DIRECTION_2,
+        ).all()
+        context['route_station_groups'] = [route_1, route_2]
         return context
