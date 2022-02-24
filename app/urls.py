308dd965b6b921e7f34bf3fb36015e84b45dbb9f
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2022-02-24 15:14:10 -0500

    Add sample bootstrap template for home page and show stations in admin

--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
 
 urlpatterns = [
+    path('', include('routes.urls', namespace='routes')),
     path('admin/', admin.site.urls),
 ]
--- routes/admin.py
+++ routes/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.utils.html import mark_safe
 
 from .models import Route
+from .models import BusStation
+from .models import RouteStations
+
+
+class RouteStationsInline(admin.TabularInline):
+    model = RouteStations
 
 
 @admin.register(Route)
@@ -20,3 +26,27 @@ class RouteAdmin(admin.ModelAdmin):
             f'border-bottom: 5px solid {obj.color};">'
             f'{obj.code}</span>'
         )
+
+
+@admin.register(BusStation)
+class BusStationAdmin(admin.ModelAdmin):
+    search_fields = ('code', 'name')
+    list_display = (
+        'code',
+        'name',
+        'created_at',
+        'updated_at',
+    )
+
+
+@admin.register(RouteStations)
+class RouteStationsAdmin(admin.ModelAdmin):
+    list_display = (
+        'route',
+        'direction',
+        'position',
+        'bus_station',
+        'created_at',
+        'updated_at',
+    )
+    list_select_related = ('route', 'bus_station')
